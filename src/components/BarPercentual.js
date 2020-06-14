8244094d2517a0be5d45651cc7e429820d5af0d7
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-14 20:14:58 -0300

    Added individual bar logic

--- src/components/BarPercentual.js
+++ src/components/BarPercentual.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react'
 
 export default class BarPercentual extends Component {
 
-    render() {
-        const { currentValue, currentColor} = this.props;
+    constructor() {
+        super();
+        this.state = {
+            innerNumber: ''
+        }
+    }
 
+    handleValueShow(stringNumber) {
+        this.setState({
+            ...this.state,
+            innerNumber: stringNumber
+        });
+
+    }
+
+    render() {
+        const { currentValue, currentColor } = this.props;
+        const { innerNumber } = this.state;
         return (
-            <div style={{
-                marginTop: '4vh',
-                width: currentValue + '%',
-                height: '4vh',
-                backgroundColor: `${currentColor}`
-            }}
+            <div
+                style={{
+                    marginTop: '4vh',
+                    width: currentValue + '%',
+                    height: '4vh',
+                    backgroundColor: `${currentColor}`,
+                }}
+                onMouseOver={() => { this.handleValueShow(String(currentValue)) }}
+                onMouseLeave={() => { this.handleValueShow('') }}
             >
+                <h2 style={{
+                    backgroundColor: `${currentColor}`,
+
+                }}>
+                    {`${innerNumber}`}
+                </h2>
             </div>
         )
     }
